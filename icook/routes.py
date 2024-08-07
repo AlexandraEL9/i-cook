cb4a9baca4a11f3b01f6d7a8106883a1aabef74d
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-07 12:48:40 +0000

    add: add categories page and route to python file

--- icook/routes.py
+++ icook/routes.py
@@ -6,3 +6,7 @@ from icook.models import Category, Recipe
 @app.route("/")
 def home():
     return render_template("recipes.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
