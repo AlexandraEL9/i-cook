56fd8334c33221e12d677a7b15a3d595218506a4
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-07 12:24:49 +0000

    feat: add materialize navbar and mobile side navbar

---
+++ icook/static/js/script.js
@@ -0,0 +1,6 @@
+//navbar js
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});
